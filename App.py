37162bf9d843210f59ae7d7491efab8667ae6130
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2025-06-12 14:56:04 -0300

    Welcome to the Flask App

--- App.py
+++ App.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    name = request.args.get('name', 'World')
-    return jsonify(message=f'Hello, {name}!')
+@app.route('/')
+def principal():
+    return 'Welcome to the Flask App!'
+if __name__ == '__main__':
+    app.run(debug=True)
