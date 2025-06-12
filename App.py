50dc43d0f9eb94c3191d7daa00935c49a8e1434f
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2025-06-12 14:20:58 -0300

    Flask

---
+++ App.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    name = request.args.get('name', 'World')
+    return jsonify(message=f'Hello, {name}!')
