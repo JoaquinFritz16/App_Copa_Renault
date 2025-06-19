0f9880a956e9d168b4be49c3f022d3cd2305603e
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2025-06-19 11:16:42 -0300

    Avance para que el negro lo ponga lindo

--- App.py
+++ App.py
@@ -1,8 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def principal():
-    return 'Welcome to the Flask App!'
+    return render_template('index.html')
+
+@app.route('/pedro')
+def pedro():
+    data = {
+        "title": "Pedro"
+    }
+    return render_template('pedro.html', data=data)
 if __name__ == '__main__':
     app.run(debug=True)
